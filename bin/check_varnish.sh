3baf4c559237d9ed249ae33886037ec84ab1a634
Frederic Descamps <lefred.descamps@gmail.com>
2020-10-01 15:53:37 +0200

    fix bash errors
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- bin/check_varnish.sh
+++ bin/check_varnish.sh
@@ -4,7 +4,8 @@ BACKEND=$1
 
 MY_STATUS=$(/bin/varnishadm backend.list $BACKEND 2>/dev/null | tail -n1)
 
-if [[ $(echo "$MY_STATUS" | grep 'Healthy' >/dev/null) == 0 ]]
+echo "$MY_STATUS" | grep 'Healthy' >/dev/null
+if [[ $? -eq 0 ]]
 then
     echo "OK: $MY_STATUS"
     exit 0
